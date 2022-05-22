d36c7a7091bde04615667aeee68ec199d2fb3a6b
Eryson Miranda <eryson.ktm@gmail.com>
2022-05-22 15:38:13 -0300

    feat: Created the category routes and the rest of the product routes

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,19 @@
 import { Router } from 'express';
 import ProductsController from './app/controllers/ProductsController';
+import CategoriesController from './app/controllers/CategoriesController';
 
 const routes = new Router();
 
 routes.get('/products', ProductsController.getAll);
+routes.get('/products/filter', ProductsController.getByFilter);
+routes.post('/products', ProductsController.create);
+routes.put('/products/:id', ProductsController.update);
+routes.delete('/products/:id', ProductsController.delete);
+
+routes.get('/categories', CategoriesController.getAll);
+routes.get('/categories/filter', CategoriesController.getByFilter);
+routes.post('/categories', CategoriesController.create);
+routes.put('/categories/:id', CategoriesController.update);
+routes.delete('/categories/:id', CategoriesController.delete);
 
 export default routes;
